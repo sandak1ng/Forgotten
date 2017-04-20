bf3cdada98e7598df674bab6de1afa9cce23fd03
sandak1ng <dsan.work@gmail.com>
2017-04-20 23:58:09 +0100

    Latest update of the game with Points system included

---
+++ ForgottenWithPointsSystem/Assets/Main Menu/menu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour
+{
+
+	public void StartGame ()
+	{
+		PlayerPrefs.SetInt ("CurrentScore", 0);
+
+		Application.LoadLevel ("forgotten");
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();          
+	}
+		
+	public void DisableBoolInAnimator(Animator anim) {
+		anim.SetBool ("isDisplayed", false);
+	}
+
+	public void EnableBoolInAnimator(Animator anim) {
+		anim.SetBool ("isDisplayed", true);
+	}
+}

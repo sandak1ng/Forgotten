11ca6d7216d2e62bb444deea256aa9c02a0cfca7
U-BABY\Cakeydoodoo <tvu010@campus.goldsmiths.ac.uk>
2017-04-26 21:55:16 +0100

    animation added to dungeon

---
+++ ForgottenWithMainMenu/ForgottenWithMainMenu/Assets/Main Menu/menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour
+{
+
+	public void StartGame ()
+	{
+		Application.LoadLevel ("forgotten");
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();          
+	}
+		
+	public void DisableBoolInAnimator(Animator anim) {
+		anim.SetBool ("isDisplayed", false);
+	}
+
+	public void EnableBoolInAnimator(Animator anim) {
+		anim.SetBool ("isDisplayed", true);
+	}
+}
--- ForgottenWithPointsSystem/Assets/Dungeon/Scripts/PlayerController.cs
+++ ForgottenWithPointsSystem/Assets/Dungeon/Scripts/PlayerController.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+    private bool facingRight;
+    private Animator anim;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        facingRight = true; 
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw ("Horizontal") < -0.5f) 
 		{
 			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime, 0f, 0f));
-		}
+
+        }
 
 		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw ("Vertical") < -0.5f) 
 		{
 			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * speed * Time.deltaTime, 0f));
 		}
+        anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
+        anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
 	}
+     
 }

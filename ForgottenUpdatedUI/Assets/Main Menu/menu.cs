fd43d53bb86544cef726f881d2ebbe68a1b5923e
sandak1ng <dsan.work@gmail.com>
2017-04-30 22:07:45 +0100

    Updated UI across the game (main menu, points overlay and pause menu)

---
+++ ForgottenUpdatedUI/Assets/Main Menu/menu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menu : MonoBehaviour
+{
+	public GameObject PlayerObject;
+
+	public void StartGame ()
+	{
+		PlayerPrefs.SetInt ("CurrentScore", 0);
+		PlayerPrefs.SetFloat ("playerX", 11);
+		PlayerPrefs.SetFloat ("playerY", 95);
+		PlayerPrefs.SetFloat ("playerZ", -63);
+		Application.LoadLevel ("forgotten");
+	}
+
+	public void ExitGame ()
+	{
+		Application.Quit ();          
+	}
+		
+	public void DisableBoolInAnimator(Animator anim) {
+		anim.SetBool ("isDisplayed", false);
+	}
+
+	public void EnableBoolInAnimator(Animator anim) {
+		anim.SetBool ("isDisplayed", true);
+	}
+}

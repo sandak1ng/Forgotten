ed640374034612cc366e1f73421275a482c5a095
sandak1ng <dsan.work@gmail.com>
2017-05-04 21:49:17 +0100

    Added endscreen to pause menu

---
+++ ForgottenUpdatedUI/Assets/Dungeon/Scripts/Pause.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject PauseMenu;
+
+	private bool paused = false;
+
+	void Start () 
+	{
+		PauseMenu.SetActive (false);
+	}
+
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Pause")) 
+		{
+			paused = !paused;
+		}
+
+		if (paused) 
+		{
+			PauseMenu.SetActive (true);
+			Time.timeScale = 0;
+		}
+
+		if (!paused) 
+		{
+			PauseMenu.SetActive (false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Continue () {
+		paused = false;
+	}
+
+	public void Exit () {
+		Application.LoadLevel ("EndingScreen");
+	}
+}
